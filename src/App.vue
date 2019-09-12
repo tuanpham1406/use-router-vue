2851631ec0c34397265150995413c0bfea6c20a5
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-12 16:27:28 +0700

    Master: update template header

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6">
-          <h1 class="text-center">Vue Routing</h1>
-          <hr>
-          <router-view></router-view>
+    <div>
+        <page-header></page-header>
+        <div class="container col-md-10">
+            <router-view></router-view>
         </div>
-      </div>
     </div>
 </template>
 
 <script>
-    export default {
-      data() {
-        return {
+    import Header from "@/components/layout/Header";
 
+    export default {
+        component: {
+            pageHeader: Header
         }
-      }
     }
 </script>
 

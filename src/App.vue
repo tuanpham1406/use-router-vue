c4e313ba0e38d45e551d5df54fa0de4ee5b910da
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-12 13:16:34 +0700

    Master: update template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6">
+          <h1 class="text-center">Vue Routing</h1>
+          <hr>
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    export default {
+      data() {
+        return {
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+        }
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

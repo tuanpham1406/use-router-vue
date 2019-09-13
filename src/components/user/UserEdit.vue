a0e21c2ae007a3b76706633889bfe11e96a11112
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 11:42:38 +0700

    Master: update và truyền dữ liệu template - demo query

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
         <h1>User Edit</h1>
+        <p>User Info Client:</p>
+        <ul>
+            <li>Locate: {{$route.query.locate}}</li>
+            <li>Locate: {{$route.query.browser}}</li>
+        </ul>
     </div>
 </template>
 

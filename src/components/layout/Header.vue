97bef5546f2ed2d5546875630abf30c3f06f9e3a
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 09:44:09 +0700

    Master: create template user detail

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -9,7 +9,8 @@
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <router-link tag="li" class="nav-item" to="/" active-class="active" exact><a class="nav-link">Home</a></router-link>
-                <router-link tag="li" class="nav-item" to="/user" active-class="active"><a class="nav-link">User</a></router-link>
+                <router-link tag="li" class="nav-item" to="/user/1" active-class="active"><a class="nav-link">User</a></router-link>
+                <router-link tag="li" class="nav-item" to="/user/10" active-class="active"><a class="nav-link">Admin</a></router-link>
             </ul>
             <form class="form-inline my-2 my-lg-0">
                 <input class="form-control mr-sm-2" type="search" placeholder="search...">

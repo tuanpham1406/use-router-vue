a678f38856e799b34c435440d2e32e4449c422b8
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 11:49:41 +0700

    Master: create 404 ngoại lệ, update template, add css - demo query

---
+++ src/components/404.vue
@@ -0,0 +1,18 @@
+<template>
+    <div  class="text-center">
+        <h3>Errors</h3>
+        <p>Have some errors ...</p>
+        <button class="btn btn-primary" @click="$router.push({name: 'homepage'})">Back to Home</button>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+    .text-center {
+        color: red;
+    }
+</style>

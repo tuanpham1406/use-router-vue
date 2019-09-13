ba807775b2541c3c6b749c15eb86e6d2ecb59a4b
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 10:43:49 +0700

    Master: update template- children route

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,17 +1,11 @@
 <template>
-    <div>
-        <h1>User</h1>
-        <p>It is a long established fact that a reader will be distracted
-            by the readable content of a page when looking at its layout.
-            The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
-            as opposed to using 'Content here, content here', making it look like readable English.
-            Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text,
-            and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
-            Various versions have evolved over the years, sometimes by accident,
-            sometimes on purpose (injected humour and the like).</p>
+    <div class="mt-3">
+        <h1 class="text-center">User Manager</h1>
+        <p class="text-center">This is area to manage user of webside</p>
         <hr>
-        <button class="btn btn-primary" @click="backHome">Go to Home</button>
-        <button class="btn btn-primary" @click="goDetail">Go to Admin Profile</button>
+        <button class="btn btn-primary btn-block" @click="backHome">Go to Home</button>
+        <hr>
+        <router-view></router-view>
     </div>
 </template>
 
@@ -21,9 +15,9 @@
             backHome() {
                 this.$router.push('/');
             },
-            goDetail() {
-                this.$router.push('/user/1')
-            }
+            // goDetail() {
+            //     this.$router.push('/user/1')
+            // }
         }
     }
 </script>

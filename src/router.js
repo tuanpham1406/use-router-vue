e559b952050fedd49a4c261f412d373c68ee5100
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 10:43:17 +0700

    Master: create children router user- children route

--- src/router.js
+++ src/router.js
@@ -2,9 +2,20 @@
 import User from "@/components/user/User";
 import Home from "@/components/Home";
 import UserDetail from "@/components/user/UserDetail";
+import UserEdit from "@/components/user/UserEdit";
+import UserStart from "@/components/user/UserStart";
 
 export const routes = [
     {path:'/', name:'homepage', component: Home},
-    {path:'/user', name:'user', component: User},
-    {path:'/user/:id', name:'userdetail', component: UserDetail},
+    {
+        path:'/user',
+        name:'user',
+        component: User,
+        children: [
+            {path: '', name: 'userstart', component: UserStart},
+            {path: ':id', name: 'userdetail', component: UserDetail},
+            {path: ':id/edit', name: 'useredit', component: UserEdit}
+        ]
+    }
+    // {path:'/user/:id', name:'userdetail', component: UserDetail},
 ];

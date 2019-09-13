a69b70545bfd3f639c699064cb92c7670cd05d17
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 15:55:01 +0700

    Master: update layout, cảnh báo mất data khi đang nhập mà muốn thoát trang

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -6,11 +6,32 @@
             <li>Locate: {{$route.query.locate}}</li>
             <li>Locate: {{$route.query.browser}}</li>
         </ul>
+        <hr>
+        <button class="btn btn-block btn-danger"
+                @click="status = true"
+        >Something</button>
+        <div style="height: 700px"></div>
+        <h3 id="vue">Learn VueJs with me</h3>
+        <div style="height: 1700px"></div>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                status: false
+            }
+        },
+        beforeRouteLeave(to, from, next) {
+            if (this.status) {
+                next()
+            } else {
+                if (confirm('Bạn có chắc chắn muốn thoát không ? Bạn chưa lưu dữ liệu của trang')) {
+                    next()
+                } else next(false)
+            }
+        }
     }
 </script>
 

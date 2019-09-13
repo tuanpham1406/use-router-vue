a97b9aff44b1b00ef6db608d18373862abcd2881
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 11:47:58 +0700

    Master: refactor code - demo query

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import {routes} from "@/router";
-import Header from "@/components/layout/Header";
 
 Vue.use(VueRouter);
-Vue.component('pageHeader', Header);
 const router = new VueRouter({
     mode: 'history',
     routes

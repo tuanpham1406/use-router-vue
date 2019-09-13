841c3b76806b498f08edc8c9690dcdfe8591551c
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 10:45:59 +0700

    Master: renamed file and update template => add list router.- children route

---
+++ src/components/user/UserStart.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>Please select one user to manage:</h3>
+        <ul class="list-group">
+            <router-link tag="li" to="/user/1" class="list-group-item" style="cursor: pointer">User 01</router-link>
+            <router-link tag="li" to="/user/2" class="list-group-item" style="cursor: pointer">User 02</router-link>
+            <router-link tag="li" to="/user/3" class="list-group-item" style="cursor: pointer">User 03</router-link>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style scoped>
+
+</style>

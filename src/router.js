201307d03dd46e39bfbd2d6d4d072eaef4479f3a
khuynhthanh <phamanhtuan1406@gmail.com>
2019-09-13 09:44:41 +0700

    Master: create routes user detail

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 //define router to component
 import User from "@/components/user/User";
 import Home from "@/components/Home";
+import UserDetail from "@/components/user/UserDetail";
 
 export const routes = [
     {path:'/', name:'homepage', component: Home},
-    {path:'/user', name:'user', component: User}
+    {path:'/user', name:'user', component: User},
+    {path:'/user/:id', name:'userdetail', component: UserDetail},
 ];
